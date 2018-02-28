7db522677290d71cc9fd96701d6106ad0fb0c940
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-02-28 20:37:01 -0300

    Call swal.resetDefaults in cleanSwalState

--- tests/support/swal.js
+++ tests/support/swal.js
@@ -3,6 +3,7 @@ const swal = require('sweetalert2')
 const initialSwalProps = Object.keys(swal)
 
 async function cleanSwalState() {
+  swal.resetDefaults()
   await swal({
     animation: false,
     title: 'clear',
