546d2aca67f377f6ae12ca899112c190bb1810a4
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-02 14:42:21 -0300

    test(api): Assert that child swal has properies of parent swal

--- tests/units/main.js
+++ tests/units/main.js
@@ -65,8 +65,9 @@ describe('sweetalert2-react-content', () => {
       expect(params.title).toEqual('foo')
       return { value: 'bar' }
     }
-    Object.assign(mockSwal, swal)
+    Object.assign(mockSwal, swal, { method: () => {} })
     const mySwal = withReactContent(mockSwal)
+    expect(mySwal.method).toEqual(mockSwal.method)
     const result = await mySwal({ title: 'foo' })
     expect(result).toEqual({ value: 'bar' })
   })
