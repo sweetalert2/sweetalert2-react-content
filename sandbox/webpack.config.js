e8b5e475d008446cfb26d4ba05885a861be35152
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-02 04:47:28 -0300

    Fix sandbox

--- sandbox/webpack.config.js
+++ sandbox/webpack.config.js
@@ -1,15 +1,9 @@
 const path = require('path')
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { ProvidePlugin, NamedModulesPlugin } = require('webpack')
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
-const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin')
-
-const appSrc = path.resolve('./lib')
-const appPackageJson = path.resolve('./package.json')
-const appNodeModules = path.resolve('./node_modules')
 
 module.exports = {
+  mode: 'development',
   devtool: 'cheap-module-source-map',
   entry: ['babel-polyfill', require.resolve('./app')],
   output: {
@@ -20,13 +14,6 @@ module.exports = {
       // Point sourcemap entries to original disk location (format as URL on Windows)
       return path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
     },
-  },
-  resolve: {
-    modules: ['node_modules'], // .concat(process.env.NODE_PATH.split(path.delimiter).filter(Boolean)),
-    extensions: ['.js'],
-    plugins: [
-      new ModuleScopePlugin(appSrc, [appPackageJson]), // Prevents users from importing files from outside of src/ (or node_modules/).
-    ],
   },
   module: {
     strictExportPresence: true,
@@ -40,17 +27,8 @@ module.exports = {
     ],
   },
   plugins: [
-    new ProvidePlugin({ React: 'react' }),
+    new webpack.ProvidePlugin({ React: 'react' }),
+    new webpack.NamedModulesPlugin(), // Add module names to factory func
     new HtmlWebpackPlugin({ inject: true }), // Generates the `index.html` file
-    new NamedModulesPlugin(), // Add module names to factory func
-    // Watcher doesn't work well if you mistype casing in a path so we use
-    // a plugin that prints an error when you attempt to do this.
-    // See https://github.com/facebookincubator/create-react-app/issues/240
-    new CaseSensitivePathsPlugin(),
-    // If you require a missing module and then `npm install` it, you still have
-    // to restart the development server for Webpack to discover it. This plugin
-    // makes the discovery automatic so you don't have to restart.
-    // See https://github.com/facebookincubator/create-react-app/issues/186
-    new WatchMissingNodeModulesPlugin(appNodeModules),
   ],
 }
