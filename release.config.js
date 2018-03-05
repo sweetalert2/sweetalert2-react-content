57fc9a5d08cc6b8d01619f50e745ea6896a80cec
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-05 13:42:59 -0300

    chore(ci): Use shorter/simpler message for release commits

--- release.config.js
+++ release.config.js
@@ -8,8 +8,7 @@ module.exports = {
     {
       path: '@semantic-release/git',
       assets: 'package.json',
-      message:
-        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      message: 'chore(release): ${nextRelease.version} [skip ci]',
     },
   ],
   publish: ['@semantic-release/npm'],
