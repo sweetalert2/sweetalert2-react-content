d29cd707d105c1bd37f30045ab46957efa4a964c
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-05 10:12:27 -0300

    fix(ci): Commit version bump in CI

--- release.config.js
+++ release.config.js
@@ -1,5 +1,16 @@
+/* eslint-disable no-template-curly-in-string */
 /* eslint-env node */
 
 module.exports = {
   debug: true,
+  prepare: [
+    '@semantic-release/npm',
+    {
+      path: '@semantic-release/git',
+      assets: 'package.json',
+      message:
+        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ],
+  publish: ['@semantic-release/npm'],
 }
