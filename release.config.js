951b3fa71be5684f4fa511546bc1471d88fa8297
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-07 13:38:12 -0300

    chore(ci): Add changelog generation

--- release.config.js
+++ release.config.js
@@ -3,11 +3,17 @@
 
 module.exports = {
   debug: true,
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
   prepare: [
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     {
       path: '@semantic-release/git',
-      assets: 'package.json',
+      assets: ['package.json', 'CHANGELOG.md'],
       message: 'chore(release): ${nextRelease.version} [skip ci]',
     },
   ],
