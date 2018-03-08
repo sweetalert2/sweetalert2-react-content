b95b056b67b4a471eae2297b892645e5e14b1ef6
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-03-08 18:22:34 -0300

    feat(package): include non-minified build and use as package.main

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -1,32 +1,39 @@
 /* eslint-env node */
 const path = require('path')
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: [require.resolve('./src/sweetalert2-react-content')],
-  output: {
-    library: 'sweetalert2ReactContent',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'sweetalert2-react-content.umd.js',
-  },
-  module: {
-    strictExportPresence: true,
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: require.resolve('babel-loader'),
-        options: {},
-      },
-    ],
-  },
-  externals: expandExternalsConfig({
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    sweetalert2: 'swal',
-  }),
+module.exports = [
+  getWebpackConfig({ minify: true }),
+  getWebpackConfig({ minify: false }),
+]
+
+function getWebpackConfig({ minify }) {
+  return {
+    mode: minify ? 'production' : 'development',
+    devtool: 'source-map',
+    entry: [require.resolve('./src/sweetalert2-react-content')],
+    output: {
+      library: 'sweetalert2ReactContent',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'dist'),
+      filename: `sweetalert2-react-content.${minify ? 'umd.min' : 'umd'}.js`,
+    },
+    module: {
+      strictExportPresence: true,
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: require.resolve('babel-loader'),
+          options: {},
+        },
+      ],
+    },
+    externals: expandExternalsConfig({
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      sweetalert2: 'swal',
+    }),
+  }
 }
 
 function expandExternalsConfig(compactConfig) {
