88e9088234455d785d594e9a2161e7ba7cddb8b7
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-02-20 18:20:44 -0300

    chore(semantic-release): restore use of release-notes-generator

--- release.config.js
+++ release.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   debug: true,
   plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
