f49a2017d0d05a8d8aa1cc5b512f6a3534417ba9
Matthew Francis Brunetti <zenflow87@gmail.com>
2019-02-20 17:23:19 -0300

    fix(banner): fix version number in banner comment, closes #48 (#74)

--- release.config.js
+++ release.config.js
@@ -1,18 +1,9 @@
 module.exports = {
   debug: true,
-  verifyConditions: [
+  plugins: [
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
+    '@semantic-release/github',
   ],
-  prepare: [
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    {
-      path: '@semantic-release/git',
-      assets: ['package.json', 'CHANGELOG.md'],
-      message: 'chore(release): ${nextRelease.version} [skip ci]', // eslint-disable-line no-template-curly-in-string
-    },
-  ],
-  publish: ['@semantic-release/npm', '@semantic-release/github'],
 }
