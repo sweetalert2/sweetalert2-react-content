59a1369dc0828df8270b2e8d282f6d4da4a4e34e
Limon Monte <6059356+limonte@users.noreply.github.com>
2023-10-30 17:34:42 +0200

    chore: upd sandbox to use input

--- test/sandbox.jsx
+++ test/sandbox.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import ReactDOM from 'react-dom/client'
+import React, { useState } from 'react'
 import Swal from 'sweetalert2/dist/sweetalert2.all'
 import withReactContent from '../src/index'
 
 let MySwal = withReactContent(Swal)
 
-MySwal.fire({
-  icon: 'success',
-  title: <i>Test</i>,
-  html: <i>Hello World</i>,
-})
+function App() {
+  const [inputValue, setInputValue] = useState('')
+
+  function showSwal() {
+    MySwal.fire({
+      title: <i>Input something</i>,
+      input: 'text',
+      inputValue,
+      confirmButtonText: <i>Submit</i>,
+      preConfirm: () => {
+        setInputValue(Swal.getInput().value || '')
+      },
+    })
+  }
+
+  return (
+    <>
+      <button onClick={() => showSwal()}>show swal</button>
+      <div>input value: {inputValue}</div>
+    </>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
